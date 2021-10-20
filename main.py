ee39b879a2f18846fcb7564c1125e799fa35228b
tllau <tllau0310@gmail.com>
2021-10-20 23:06:37 +0800

    checking system complete, need for testing

--- main.py
+++ main.py
@@ -20,6 +20,9 @@ class SmallBoard(object):
         
     def get_row(self, rn):
         return self.board[rn]
+    
+    def get_column(self, cn):
+        return self.board[:, cn]
         
     def input_number(self, i, j, v):
         if v in range(1,10):
@@ -35,18 +38,39 @@ class SmallBoard(object):
 class WholeBoard(object):
     def __init__(self, board=[[SmallBoard(), SmallBoard(), SmallBoard()] for _ in range(3)]):
         self.board = board
-        print(self.board)
-
-# rn: row_number 0-8    
-    def is_row_correct(self, rn):
-        pass
         
-    def is_column_valid(self, cn):
-        pass
+    def get_smallboard(self, i, j):
+        return self.board[i][j]
+
+    # rn: row_number 0-8    
+    def is_row_correct(self, rn):
+        row = np.array([], dtype=int)
+        for col in range(3):
+            row = np.append(row, self.get_smallboard(rn // 3 , col).get_row(rn % 3))
+        return len(np.unique(row)) == 9 and 0 not in np.unique(row)        
+        
+    def is_column_correct(self, cn):
+        col = np.array([], dtype=int)
+        for row in range(3):
+            col = np.append(col, self.get_smallboard(row, cn // 3).get_column(cn % 3))
+        return len(np.unique(col)) == 9 and 0 not in np.unique(col)          
+        
+    def is_wholeboard_correct(self):
+        for a in range(9):
+            if not self.is_row_correct(a) or not self.is_column_correct(a):
+                return False
+        for i in range(3):
+            for j in range(3):
+                if not self.get_smallboard(i,j).is_board_correct():
+                    return False
+        return True
             
         
     
 
 if __name__ == "__main__":
-    b = WholeBoard()
-    sb = SmallBoard()
+    sb = SmallBoard(np.array([[1,2,3], [4,5,6],[7,8,9]], dtype=int))
+    b = WholeBoard([[sb, sb, sb] for _ in range(3)])
+    print(b.is_wholeboard_correct())
+    
+    
